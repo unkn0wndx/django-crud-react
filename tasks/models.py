a7f9bc8019e6cd33f43e20c7761a279b5e49b179
Brian Cruz Sanchez <briancs1999@outlook.com>
2023-05-02 21:11:37 -0600

    I changed the model and replaced MySQL for SQLite

--- tasks/models.py
+++ tasks/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 # Create your models here.
+    
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     done = models.BooleanField(default=False)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
