7f2da7e26c27b647594ad6ba86335d42b7cb12b3
Brian Cruz Sanchez <briancs1999@outlook.com>
2023-05-08 13:01:36 -0600

    Name changed

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,10 +1,43 @@
 import { Link } from 'react-router-dom'
+import { Grid, Text } from '@nextui-org/react'
 
 export function Navigation() {
   return (
     <>
       <Link to="/tasks">
-        <h1>Task App</h1>
+        <Grid.Container justify='center'>
+          <Text
+            h1
+            size={60}
+            css={{
+              textGradient: "45deg, $blue600 -20%, $pink600 50%",
+            }}
+            weight="bold"
+          >
+            To-
+          </Text>
+          <Text
+            h1
+            size={60}
+            css={{
+              textGradient: "45deg, $purple600 -20%, $pink600 100%",
+            }}
+            weight="bold"
+          >
+            Do&nbsp;
+          </Text>
+          <Text
+            h1
+            size={60}
+            css={{
+              textGradient: "45deg, $yellow600 -20%, $red600 100%",
+            }}
+            weight="bold"
+          >
+            App
+          </Text>
+        </Grid.Container>
+
       </Link>
       <Link to="/tasks-create">create task</Link>
     </>
