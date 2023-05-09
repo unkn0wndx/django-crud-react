eea24e964e2023479587635e7706c9002719e7aa
Brian Cruz Sanchez <briancs1999@outlook.com>
2023-05-09 12:55:54 -0600

    new design

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -4,7 +4,7 @@ import { updateTask } from '../api/tasks.api';
 import { toast } from 'react-hot-toast';
 import confetti from 'canvas-confetti';
 
-export function TaskCard({ task }) {
+export function TaskCard({ task, setHelper }) {
 
   const date = new Date(task.updated).toString().substring(0, 24);
 
@@ -19,60 +19,52 @@ export function TaskCard({ task }) {
   }
 
   const onSubmit = async (status) => {
+    setHelper(true);
     status && handleConfetti();
     const data = { done: status, title: task.title, description: task.description, category: task.category }
     await updateTask(task.id, data);
     toast.success(`Task status has changed`, {
-      // position: 'bottom-right',
       style: {
         background: '#101010',
         color: '#fff'
       }
     })
-
   }
 
   return (
-    <div style={{ background: '#101010' }}>
-      <Grid.Container gap={2}>
-        <Grid sm={12} md={5}>
-          <Card css={{ p: "$6", mw: "400px" }}>
-            <Card.Header>
-              <Checkbox isRounded color="success" labelColor="success" size="xl" aria-label='' onChange={onSubmit} defaultSelected={task.done} />
-              <Grid.Container css={{ pl: "$6" }}>
-                <Grid xs={12}>
-                  <Text h4 css={{ lineHeight: "$xs" }}>
-                    {task.title}
-                  </Text>
-                </Grid>
-                <Grid xs={12}>
-                  <Text css={{ color: "$accents8" }}>{date}</Text>
-                </Grid>
-              </Grid.Container>
-            </Card.Header>
-            <Card.Body css={{ py: "$2" }}>
-              <Text>
-                {task.description}
-              </Text>
-            </Card.Body>
-            <Card.Footer>
-              <Row justify="space-around">
-                <Grid xs={12}>
-                  <Badge color="success" variant="bordered" size="lg">
-                    Importante
-                  </Badge>
-                </Grid>
-                <Grid xs={12} alignItems='center'>
-                  <Button color="default" shadow rounded size="sm" onClick={() => {
-                    navigate(`/tasks/${task.id}`)
-                  }}>Edit</Button>
-                </Grid>
-              </Row>
-            </Card.Footer>
-          </Card>
-        </Grid>
-      </Grid.Container>
-    
-    </div>
+    <Card css={{ p: "$6", maxHeight: "250px" }}>
+      <Card.Header>
+        <Checkbox isRounded color="success" labelColor="success" size="xl" aria-label='' onChange={onSubmit} defaultSelected={task.done} />
+        <Grid.Container css={{ pl: "$6" }}>
+          <Grid xs={12}>
+            <Text h4 css={{ lineHeight: "$xs" }}>
+              {task.title}
+            </Text>
+          </Grid>
+          <Grid xs={12}>
+            <Text css={{ color: "$accents8" }}>{date}</Text>
+          </Grid>
+        </Grid.Container>
+      </Card.Header>
+      <Card.Body css={{ py: "$2" }}>
+        <Text>
+          {task.description}
+        </Text>
+      </Card.Body>
+      <Card.Footer>
+        <Grid.Container justify="space-around">
+          <Grid xs={6}>
+            <Badge color="success" variant="bordered" size="lg">
+              Importante
+            </Badge>
+          </Grid>
+          <Grid xs={6}>
+            <Button color="default" shadow rounded size="sm" onClick={() => {
+              navigate(`/tasks/${task.id}`)
+            }}>Edit</Button>
+          </Grid>
+        </Grid.Container>
+      </Card.Footer>
+    </Card>
   )
 }
