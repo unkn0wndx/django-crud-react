e9ce88d5245e6956d2a4190aedca57d38b316f86
Brian Cruz Sanchez <briancs1999@outlook.com>
2023-05-09 13:08:20 -0600

    New design

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,45 +1,51 @@
 import { Link } from 'react-router-dom'
-import { Grid, Text } from '@nextui-org/react'
+import { Divider, Grid, Text } from '@nextui-org/react'
 
 export function Navigation() {
   return (
     <>
-      <Link to="/tasks">
-        <Grid.Container justify='center'>
-          <Text
-            h1
-            size={60}
-            css={{
-              textGradient: "45deg, $blue600 -20%, $pink600 50%",
-            }}
-            weight="bold"
-          >
-            To-
-          </Text>
-          <Text
-            h1
-            size={60}
-            css={{
-              textGradient: "45deg, $purple600 -20%, $pink600 100%",
-            }}
-            weight="bold"
-          >
-            Do&nbsp;
-          </Text>
-          <Text
-            h1
-            size={60}
-            css={{
-              textGradient: "45deg, $yellow600 -20%, $red600 100%",
-            }}
-            weight="bold"
-          >
-            App
-          </Text>
-        </Grid.Container>
+      <Divider />
+      <Grid.Container justify='space-between' css={{ p: 20 }}>
+        <Link to="/tasks">
+          <Grid.Container justify='center'>
+            <Text
+              h1
+              size={60}
+              css={{
+                textGradient: "45deg, $blue600 -20%, $pink600 50%",
+              }}
+              weight="bold"
+            >
+              To-
+            </Text>
+            <Text
+              h1
+              size={60}
+              css={{
+                textGradient: "45deg, $purple600 -20%, $pink600 100%",
+              }}
+              weight="bold"
+            >
+              Do&nbsp;
+            </Text>
+            <Text
+              h1
+              size={60}
+              css={{
+                textGradient: "45deg, $yellow600 -20%, $red600 100%",
+              }}
+              weight="bold"
+            >
+              App&nbsp;
+            </Text>
+          </Grid.Container>
+        </Link>
 
-      </Link>
-      <Link to="/tasks-create">create task</Link>
+        <Grid>
+          <Link to="/tasks-create"><Text h1 size={60}>Click to create a task</Text></Link>
+        </Grid>
+      </Grid.Container>
+      <Divider />
     </>
   )
 }
